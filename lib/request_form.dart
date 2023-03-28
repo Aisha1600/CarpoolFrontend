5d6d6be19f1afa892d78e596113680f3f7a1f74b
Aisha1600 <aishataif64@gmail.com>
2023-03-28 17:02:43 +0500

    changes

--- lib/request_form.dart
+++ lib/request_form.dart
@@ -3,47 +3,33 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class RequestForm extends StatefulWidget {
-  const RequestForm({super.key});
+  RequestForm({Key? key});
 
   @override
   State<RequestForm> createState() => _RequestFormState();
 }
 
 class _RequestFormState extends State<RequestForm> {
+  bool _isHovering = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
+    return Builder(
+      builder: (context) => MouseRegion(
+        onEnter: (event) {
+          setState(() {
+            _isHovering = true;
+          });
+        },
+        onExit: (event) {
+          setState(() {
+            _isHovering = false;
+          });
+        },
         child: Container(
-          padding: EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 2,
-                blurRadius: 5,
-                offset: Offset(0, 3),
-              ),
-            ],
-            color: Colors.white,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Text(
-                    'Time',
-                    style: TextStyle(
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.normal,
-                        fontFamily: 'Palanquin'),
-                  )
-                ],
-              )
-            ],
-          ),
+          color: _isHovering ? Colors.blue : Colors.red,
+          width: 100,
+          height: 100,
         ),
       ),
     );
