9a1a55ade5b5336be839f9fbc18365a2ea524fce
Aisha1600 <aishataif64@gmail.com>
2023-04-13 15:54:16 +0500

    fixed merge errors

--- lib/log_in.dart
+++ lib/log_in.dart
@@ -5,17 +5,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 // ignore: unnecessary_import
 
-
-
 class LoginForm {
   late final String password;
   late final String email;
 
-  LoginForm(
-      {
-      required this.password,
-      required this.email,
-       });
+  LoginForm({
+    required this.password,
+    required this.email,
+  });
   Map<String, dynamic> toJson() {
     return {
       'password': password,
@@ -30,6 +27,7 @@ class LogIn extends StatefulWidget {
   @override
   State<LogIn> createState() => _LogIn();
 }
+
 class _LogIn extends State<LogIn> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
@@ -42,90 +40,10 @@ class _LogIn extends State<LogIn> {
         title: Text(title),
       ),
       body: Center(
-<<<<<<< HEAD
         child: ListView(children: <Widget>[
           Container(
               alignment: Alignment.center,
               padding: const EdgeInsets.all(13),
-=======
-          child: ListView(children: <Widget>[
-        Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(13),
-            child: const Text(
-              'Log In',
-              style: TextStyle(
-                color: Color(0xFF05998c), // Will work,
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-              ),
-            )),
-        Container(
-          padding: const EdgeInsets.fromLTRB(6, 6, 6, 6),
-          child: TextField(
-            controller: nameController,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Email',
-            ),
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.fromLTRB(6, 6, 6, 6),
-          child: TextField(
-            obscureText: true,
-            controller: passwordController,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
-            ),
-          ),
-        ),
-        SizedBox(
-            child: TextButton(
-              //changes here
-          onPressed:() async {
-
-            // LoginForm form = LoginForm(
-            //   email: nameController.text,
-            //   password: passwordController.text);
-            //   try {
-                  
-            //       final jsonData = jsonEncode(form.toJson());
-            //       print(jsonData);
-            //       print(json.decode(jsonData));
-            //       final response = await http.post(
-            //         //URL LOCAL HOST NEEDS TO BE CHANGED
-            //         Uri.parse('http://localhost:5000/member/login'),
-            //         headers: {'Content-Type': 'application/json'},
-            //         body: jsonData,
-            //       );
-              //    print(response.statusCode);
-              //     if (response.statusCode == 200) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ForgotPassword()),
-            );
-               //   print(json.decode(response.body));
-              //    }
-          //  } catch (error) {
-             //     print(error);
-             //   }
-          },
-          child: const Text(
-            'Forgot Password?',
-            style: TextStyle(
-              color: Colors.blue, // Will work,
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        )),
-        Container(
-            height: 60,
-            padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
-            child: ElevatedButton(
->>>>>>> 8c49aa0ef18680a10f5f336aeea7bb608f8b63c1
               child: const Text(
                 'Log In',
                 style: TextStyle(
@@ -135,77 +53,124 @@ class _LogIn extends State<LogIn> {
                 ),
               )),
           Container(
-            padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+            padding: const EdgeInsets.fromLTRB(6, 6, 6, 6),
             child: TextField(
               controller: emailController,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Email',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 8, horizontal: 8),
               ),
-<<<<<<< HEAD
             ),
           ),
           Container(
-            padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+            padding: const EdgeInsets.fromLTRB(6, 6, 6, 6),
             child: TextField(
-              controller: passwordController,
               obscureText: true,
+              controller: passwordController,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Password',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 8, horizontal: 8),
               ),
             ),
           ),
           SizedBox(
               child: TextButton(
-            onPressed: () {
+            //changes here
+            onPressed: () async {
+              // LoginForm form = LoginForm(
+              //   email: nameController.text,
+              //   password: passwordController.text);
+              //   try {
+
+              //       final jsonData = jsonEncode(form.toJson());
+              //       print(jsonData);
+              //       print(json.decode(jsonData));
+              //       final response = await http.post(
+              //         //URL LOCAL HOST NEEDS TO BE CHANGED
+              //         Uri.parse('http://localhost:5000/member/login'),
+              //         headers: {'Content-Type': 'application/json'},
+              //         body: jsonData,
+              //       );
+              //    print(response.statusCode);
+              //     if (response.statusCode == 200) {
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const ForgotPassword()),
               );
+              //   print(json.decode(response.body));
+              //    }
+              //  } catch (error) {
+              //     print(error);
+              //   }
             },
-=======
-              onPressed: () async {
-                 LoginForm form = LoginForm(
-              email: nameController.text,
-              password: passwordController.text);
-              try {
-                  
-                  final jsonData = jsonEncode(form.toJson());
-                  print(jsonData);
-                  print(json.decode(jsonData));
-                  final response = await http.post(
-                    //URL LOCAL HOST NEEDS TO BE CHANGED
-                    Uri.parse('http://localhost:5000/member/login'),
-                    headers: {'Content-Type': 'application/json'},
-                    body: jsonData,
-                  );
-                  print(response.statusCode);
-                   if (response.statusCode == 200) {
-            Navigator.push(
-              context, // fix navigation for login
-              MaterialPageRoute(builder: (context) => const ForgotPassword()),
-            );
-                  print(json.decode(response.body));
+            child: const Text(
+              'Forgot Password?',
+              style: TextStyle(
+                color: Colors.blue, // Will work,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )),
+          Container(
+            height: 60,
+            padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
+            child: ElevatedButton(
+                onPressed: () async {
+                  LoginForm form = LoginForm(
+                      email: emailController.text,
+                      password: passwordController.text);
+                  try {
+                    final jsonData = jsonEncode(form.toJson());
+                    print(jsonData);
+                    print(json.decode(jsonData));
+                    final response = await http.post(
+                      //URL LOCAL HOST NEEDS TO BE CHANGED
+                      Uri.parse('http://localhost:5000/member/login'),
+                      headers: {'Content-Type': 'application/json'},
+                      body: jsonData,
+                    );
+                    print(response.statusCode);
+                    if (response.statusCode == 200) {
+                      Navigator.push(
+                        context, // fix navigation for login
+                        MaterialPageRoute(
+                            builder: (context) => const ForgotPassword()),
+                      );
+                      print(json.decode(response.body));
+                    }
+                  } catch (error) {
+                    print(error);
                   }
-            } catch (error) {
-                  print(error);
-                }
-          
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(builder: (context) => const ForgotPassword()),
-                // );
-              },
-            )),
-        Container(
+
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => const ForgotPassword()),
+                  // );
+                },
+                child: const Text(
+                  'Log In',
+                  style: TextStyle(
+                    color: Color(0xFF05998c), // Will work,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                )),
+          ),
+          Container(
+              padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+              child: TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                ),
+              )),
+          Container(
             alignment: Alignment.center,
             padding: const EdgeInsets.all(8),
->>>>>>> 8c49aa0ef18680a10f5f336aeea7bb608f8b63c1
             child: const Text(
               'Forgot Password?',
               style: TextStyle(
@@ -214,7 +179,7 @@ class _LogIn extends State<LogIn> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-          )),
+          ),
           Container(
               height: 60,
               padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
